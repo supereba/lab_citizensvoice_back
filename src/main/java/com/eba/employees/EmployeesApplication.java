2b4a2da3e2dae855c4599254e6b08b57e638d597
Emmanuel Brun D'Aubignosc <emmanuel@brun>
2019-09-03 11:36:41 +0200

    First commit

---
+++ src/main/java/com/eba/employees/EmployeesApplication.java
@@ -0,0 +1,20 @@
+package com.eba.employees;
+
+/**
+ *
+ * Spring 5 + Angular 8 + MySQL CRUD tutorial sample application
+ * 
+ * https://www.javaguides.net/2019/06/spring-boot-angular-7-crud-example-tutorial.html
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesApplication.class, args);
+	}
+
+}
